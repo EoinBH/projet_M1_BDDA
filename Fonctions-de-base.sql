7add9858611bec77e6e9b55ec3ce93b445613e78
Eoin Brereton Hurley <breretoe@tcd.ie>
2025-11-22 16:15:31 +0100

    Modification aux Fonctions de base et ajout du fichier Liste-Donnees.sql

--- Fonctions-de-base.sql
+++ Fonctions-de-base.sql
@@ -8,6 +8,54 @@ CREATE OR REPLACE FUNCTION ajouter_auteur(nom TEXT, pays TEXT) RETURNS VOID AS $
 $$ LANGUAGE plpgsql;
 
 --Tests :
-SELECT ajouter_auteur('DUPONT', 'France');
+SELECT ajouter_auteur('HUGO', 'France');
 SELECT ajouter_auteur(NULL, 'France');
+SELECT ajouter_auteur('JOYCE', NULL);
+SELECT ajouter_auteur('WERBER', 'France');
+--Pour voir les données :
 SELECT * FROM auteur;
+--Supprimer les données si besoin :
+DELETE FROM auteur;
+ALTER SEQUENCE auteur_id_auteur_seq RESTART WITH 1;
+
+--Fonction qui ajoute un livre et vérifie que le nombre d'exemplaires est positif :
+CREATE OR REPLACE FUNCTION ajouter_livre(titre TEXT, id_auteur INT, id_categorie INT, annee INT, nb_exemplaires INT) RETURNS VOID AS $$
+	BEGIN
+		IF nb_exemplaires > 0 THEN
+			INSERT INTO livre(titre, id_auteur, id_categorie, annee, nb_exemplaires) VALUES (titre, id_auteur, id_categorie, annee, nb_exemplaires);
+		END IF;
+	END
+$$ LANGUAGE plpgsql;
+
+--Tests :
+--id_auteur = 1 : "WERBER"
+SELECT ajouter_livre('Les Fourmis', 1, NULL, 1991, 10);
+
+/*
+--Fonction qui ajoute un livre et vérifie que le nombre d'exemplaires est positif :
+CREATE OR REPLACE FUNCTION ajouter_livre_test(titre TEXT, nom_auteur TEXT, id_categorie INT, annee INT, nb_exemplaires INT) RETURNS VOID AS $$
+	DECLARE
+		id_aut INT;
+	BEGIN
+		SELECT id_auteur FROM auteur WHERE nom = nom_auteur INTO id_aut;
+		INSERT INTO livre(titre, id_auteur, id_categorie, annee, nb_exemplaires) VALUES (titre, id_aut, id_categorie, annee, nb_exemplaires);
+	END
+$$ LANGUAGE plpgsql;
+
+--Tests :
+SELECT ajouter_livre_test('Les Fourmis', 'WERBER', NULL, 1991, 10);
+SELECT ajouter_livre_test('Le Dernier Jour d''un Condamnné', 'HUGO', NULL, 1829, 10);
+*/
+
+--Pour voir les données :
+SELECT * FROM  livre;
+--Supprimer les données si besoin :
+DELETE FROM livre;
+ALTER SEQUENCE livre_id_livre_seq RESTART WITH 1;
+
+--Fonction qui retourne le nombre total de livres appartenant à une catégorie donnée.
+CREATE OR REPLACE FUNCTION nb_livres_categorie(id_categorie) RETURNS INT AS $$
+	BEGIN
+		
+	END
+$$ LANGUAGE plpgsql;
---
+++ Liste-Donnees.sql
@@ -0,0 +1,70 @@
+--Auteurs :
+--Bernard WERBER, France
+SELECT ajouter_auteur('WERBER', 'France');
+--James JOYCE, Irlande
+SELECT ajouter_auteur('JOYCE', 'Irlande');
+--F. Scott Fitzgerald, États-Unis
+SELECT ajouter_auteur('FITZGERALD', 'États-Unis');
+--Charles DICKENS, Angleterre
+SELECT ajouter_auteur('DICKENS', 'Angleterre');
+--William SHAKESPEARE, Angleterre
+SELECT ajouter_auteur('SHAKESPEARE', 'Angleterre');
+--George ORWELL, Angleterre
+SELECT ajouter_auteur('ORWELL', 'Angleterre');
+--Victor HUGO, France
+SELECT ajouter_auteur('HUGO', 'France');
+
+--Catégories :
+--selon https://bookvillage.app/vendre-ses-livres/type-de-livre-quels-sont-les-principaux-genres-litteraires/
+/*
+1. Le genre narratif
+– Nouvelle
+– Conte
+– Mythe
+– Légende
+– Biographi
+– Autobiographie
+– Chronique
+– Apologue
+– Journal
+– Roman :
+Les genres romanesques
+– Roman sentimentaux
+– Roman de mœurs
+– Roman d’apprentissage
+– Roman d’anticipation ou de science-fiction
+– Roman d’aventures
+– Roman philosophique
+– Roman picaresque
+– Roman policier
+– Roman historique
+– Roman d’horreur
+
+2. Le genre poétique
+- Chanson
+– Ballade
+– Calligramme
+– Chant Royal
+– Élégie
+– Épigramme
+– Épopée
+– Fatrasie
+– Ode
+
+3. Le genre théâtral
+– Tragédie
+– Comédie
+– Farce
+– Moralité
+– Drame
+– Proverbe
+
+4. Le genre épistolaire
+
+5. Le genre argumentatif
+- L’essai
+– La fable
+– Le fabliau
+– Le pamphlet
+– Le sermon
+*/
