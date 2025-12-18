420e1385a2ad9bda53c74259957e8d1e648d518c
Eoin Brereton Hurley <breretoe@tcd.ie>
2025-12-18 16:46:49 +0000

    Ajout des commentaires

--- guided_projet.sql
+++ guided_projet.sql
@@ -1,4 +1,6 @@
--- Partie 1: Fonctions de base 
+--------------------------------
+-- Partie 1: Fonctions de base
+--------------------------------
 
 --Fonction qui insère un nouvel auteur dans la table auteur :
 CREATE OR REPLACE FUNCTION ajouter_auteur(p_nom TEXT, p_pays TEXT) 
@@ -8,7 +10,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
---Tests :
+--Tests ajouter_auteur :
 SELECT ajouter_auteur('HUGO', 'France');
 SELECT ajouter_auteur(NULL, 'France');
 SELECT ajouter_auteur('JOYCE', NULL);
@@ -18,6 +20,7 @@ SELECT ajouter_auteur('WERBER', 'France');
 SELECT * FROM auteur;
 --Supprimer les données si besoin :
 DELETE FROM auteur;
+--Remettre à zéro le prochain identifiant affecté :
 ALTER SEQUENCE auteur_id_auteur_seq RESTART WITH 1;
 
 --Fonction qui ajoute un livre et vérifie que le nombre d'exemplaires est positif :
@@ -30,13 +33,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+--Tests ajouter_livre :
 SELECT ajouter_livre('Les Fourmis', 4, 2, 1991, 10); 
 SELECT ajouter_livre('Le Dernier Jour d''un Condamnné', 7, 1, 1991, 10);
 SELECT ajouter_livre('Exemple', 6, 2, 1991, 10);
 
-
---Proposition d'Eoin :
---Fonction qui ajoute un livre et vérifie que le nombre d'exemplaires est positif :
+--Fonction supplémentaire qui ajoute un livre et vérifie que le nombre d'exemplaires est positif :
+--Les paramètres sont de type TEXT !
 CREATE OR REPLACE FUNCTION ajouter_livre_texte(titre TEXT, nom_auteur TEXT, nom_categorie TEXT, annee INT, nb_exemplaires INT) RETURNS VOID AS $$
 	DECLARE
 		id_aut INT;
@@ -52,17 +55,17 @@ $$ LANGUAGE plpgsql;
 SELECT * FROM  livre;
 --Supprimer les données si besoin :
 DELETE FROM livre;
+--Remettre à zéro le prochain identifiant affecté :
 ALTER SEQUENCE livre_id_livre_seq RESTART WITH 1;
 
---Fonction qui insère une nouvelle catégorie dans la table catégorie :
+--Fonction supplémentaire qui insère une nouvelle catégorie dans la table catégorie :
 CREATE OR REPLACE FUNCTION ajouter_categorie(p_nom TEXT) RETURNS VOID AS $$
 	BEGIN
 		INSERT INTO categorie(nom) VALUES(p_nom);
 	END
 $$ LANGUAGE plpgsql;
 
-
---Test categorie:
+--Tests ajouter_categorie:
 SELECT ajouter_categorie('Biographie'); 
 SELECT ajouter_categorie('Drame');
 SELECT ajouter_categorie('Roman');
@@ -72,7 +75,7 @@ SELECT ajouter_categorie('Essai');
 --Pour voir les données :
 SELECT * FROM categorie;
 
---Fonction qui retourne le nombre total de livres appartenant à une catégorie donnée.
+--Fonction qui retourne le nombre total de livres appartenant à une catégorie donnée :
 CREATE OR REPLACE FUNCTION nb_livres_categorie(p_id_categorie INT) 
 RETURNS INT AS $$
     DECLARE
@@ -83,9 +86,37 @@ RETURNS INT AS $$
 	END
 $$ LANGUAGE plpgsql;
 
+--Tests nb_livres_categorie :
 SELECT nb_livres_categorie(3);
 
--- Partie 2: Fonctions avec boucle
+--Fonction supplémentaire qui permet d'insérer un emprunteur dans la table emprunteur :
+CREATE OR REPLACE FUNCTION ajouter_emprunteur(p_nom TEXT) RETURNS VOID AS $$
+BEGIN
+	INSERT INTO emprunteur(nom) VALUES (p_nom);
+END;
+$$ LANGUAGE plpgsql;
+
+--Fonction supplémentaire qui permet d'insérer un emprunt dans la table emprunt :
+--Cette fonction prend un entier 'duree' qui indique le nombre de jours du prêt
+CREATE OR REPLACE FUNCTION ajouter_emprunt(p_titre_livre TEXT, p_nom_emprunteur TEXT, duree INT) RETURNS VOID AS $$
+DECLARE
+	id_liv INT;
+	id_emp INT;
+	date_fin DATE = CURRENT_DATE + duree;
+BEGIN
+	SELECT id_livre FROM livre WHERE titre = p_titre_livre INTO id_liv;
+	SELECT id_emprunteur FROM emprunteur WHERE nom = p_nom_emprunteur INTO id_emp;
+	INSERT INTO emprunt(id_livre, id_emprunteur, date_retour) VALUES (id_liv, id_emp, date_fin);
+END
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION ajouter_emprunt;
+
+-----------------------------------
+-- Partie 2: Fonction avec boucle
+-----------------------------------
+
+--Fonction qui affiche tous les livres publiés avant l'année 2000 :
 CREATE OR REPLACE FUNCTION maj_annee_livres() RETURNS VOID AS $$
 	DECLARE
 		liv RECORD;
@@ -97,17 +128,19 @@ CREATE OR REPLACE FUNCTION maj_annee_livres() RETURNS VOID AS $$
 	END;
 $$ LANGUAGE plpgsql;
 
+--Tests maj_annee_livres :
 SELECT maj_annee_livres();
 
--- Partie 3: Fonctions avec curseur
+-----------------------------------
+-- Partie 3: Fonction avec curseur
+-----------------------------------
 
+--Fonction qui retourne tous les livres écrits par un auteur donnée :
 CREATE OR REPLACE FUNCTION liste_livres_auteur(nom_auteur TEXT) RETURNS SETOF tLivre AS $$
 DECLARE
 	id_aut INT;
     cursLivres CURSOR FOR SELECT id_auteur, titre FROM livre;
 	tNuplet tLivre;
-	-- Proposition d'Angela :
-	-- Utilisation d'une jointure (JOIN)
 BEGIN
     SELECT id_auteur FROM auteur WHERE nom = nom_auteur INTO id_aut;
 	OPEN cursLivres;
@@ -123,64 +156,49 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
---Création du type enregistrement tNuplet(nom TEXT, titre TEXT) :
+--Création du type enregistrement tLivre(id_aut INT, titre TEXT) :
 CREATE TYPE tLivre AS (
 	id_aut INT,
 	titre TEXT
 );
 
+--Tests liste_livres_auteur :
 SELECT liste_livres_auteur('HUGO');
 SELECT liste_livres_auteur('KING');
 SELECT liste_livres_auteur('JOYCE');
 
-CREATE OR REPLACE FUNCTION liste_livres_auteur_angela(p_nom_auteur TEXT) 
-RETURNS VOID AS $$
-DECLARE 
-    rec_livres CURSOR FOR 
-        SELECT l.titre FROM livre l
-        JOIN auteur a ON l.id_auteur = a.id_auteur
-        WHERE a.nom = p_nom_auteur;
-    v_titres TEXT;
-BEGIN
-    OPEN rec_livres;
-    LOOP
-        FETCH rec_livres INTO v_titres;
-        EXIT WHEN NOT FOUND;
-
-        RAISE NOTICE 'Livre: %', v_titres;
-    END LOOP;
-    CLOSE rec_livres;
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT liste_livres_auteur_angela('HUGO');
-
-
+-------------------------------------------
 -- Partie 4: Fonctions avec SQL dynamique
+-------------------------------------------
 
+--Fonction qui compte le nombre d'enregistrements présents dans une table donnée :
 CREATE OR REPLACE FUNCTION compter_elements(table_name TEXT) RETURNS INT AS $$
 DECLARE
     nombreEnreg INT;
 BEGIN
     EXECUTE 'SELECT COUNT (*) FROM ' || table_name INTO nombreEnreg;
-	-- Proposition d'Angela :
-	-- EXECUTE format('SELECT COUNT (*) FROM %I' || table_name INTO nombreEnreg;)
     RETURN nombreEnreg;
 END
 $$ LANGUAGE plpgsql;
 
+--Tests compter_elements :
 SELECT compter_elements('auteur');
 SELECT compter_elements('categorie');
 
+----------------------------
 -- Partie 5: Trigger guidé
+----------------------------
 
+--Création d'un trigger qui empêche l'insertion d'un emprunt si le livre n'a plus d'exemplaires :
 CREATE TRIGGER verif_disponibilite
 BEFORE INSERT ON emprunt
 FOR EACH ROW
 EXECUTE PROCEDURE verif_dispo();
 
+--Supprimer le trigger si besoin :
 DROP TRIGGER verif_disponibilite ON emprunt;
 
+--Fonction associée au trigger verif_disponibilite :
 CREATE OR REPLACE FUNCTION verif_dispo() RETURNS TRIGGER AS $$
 DECLARE
 	nombreExem INT;
@@ -194,29 +212,6 @@ BEGIN
 	RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
-
--- Plus de fonctions de base qui seront nécessaires :
-
-CREATE OR REPLACE FUNCTION ajouter_emprunteur(p_nom TEXT) RETURNS VOID AS $$
-BEGIN
-	INSERT INTO emprunteur(nom) VALUES (p_nom);
-END;
-$$ LANGUAGE plpgsql;
-
--- Cette fonction prend un entier 'duree' qui indique le nombre de jours du prêt
-CREATE OR REPLACE FUNCTION ajouter_emprunt(p_titre_livre TEXT, p_nom_emprunteur TEXT, duree INT) RETURNS VOID AS $$
-DECLARE
-	id_liv INT;
-	id_emp INT;
-	date_fin DATE = CURRENT_DATE + duree;
-BEGIN
-	SELECT id_livre FROM livre WHERE titre = p_titre_livre INTO id_liv;
-	SELECT id_emprunteur FROM emprunteur WHERE nom = p_nom_emprunteur INTO id_emp;
-	INSERT INTO emprunt(id_livre, id_emprunteur, date_retour) VALUES (id_liv, id_emp, date_fin);
-END
-$$ LANGUAGE plpgsql;
-
-DROP FUNCTION ajouter_emprunt;
 
 --------------------------------------------------
 -- Alimentation et tests de la base de données :
